d33fbf93e962a553ccb67f95254c94b8303775d8
Ruslan Zinnatov <rh26157@gmail.com>
2022-12-10 12:02:30 +0300

    update tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,6 +1,13 @@
 from gendiff.scripts.gendiff import generate_diff
 
 
+correct_diff = open('tests/fixtures/diff.txt').read()
+
+
 def test1():
     diff = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
-    assert diff == '{\n  - follow: False\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: True\n}'
+    assert diff == correct_diff
+
+def test2():
+    diff = generate_diff('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yml')
+    assert diff == correct_diff
