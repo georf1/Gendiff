be88cac2f823f1559cf4c48a98ba76502704fa72
Ruslan Zinnatov <rh26157@gmail.com>
2022-12-17 10:34:22 +0300

    update tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,13 +1,25 @@
 from gendiff.scripts.gendiff import generate_diff
 
 
-correct_diff1 = open('tests/fixtures/diff1.txt').read()
-correct_diff2 = open('tests/fixtures/diff2.txt').read()
+correct_diff_stylish1 = open('tests/fixtures/diff1.txt').read()
+correct_diff_stylish2 = open('tests/fixtures/diff2.txt').read()
+
+correct_diff_plain1 = open('tests/fixtures/diff3.txt').read()
+correct_diff_plain2 = open('tests/fixtures/diff4.txt').read()
+
 
 def test1():
     diff = generate_diff('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yml')
-    assert diff == correct_diff1
+    assert diff == correct_diff_stylish1
 
 def test2():
     diff = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
-    assert diff == correct_diff2
+    assert diff == correct_diff_stylish2
+
+def test3():
+    diff = generate_diff('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yml', 'plain')
+    assert diff == correct_diff_plain1
+
+def test4():
+    diff = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'plain')
+    assert diff == correct_diff_plain2
