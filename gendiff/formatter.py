cefcb02c89c8e1f577ee4a6669e9eddbf441b4ca
Ruslan Zinnatov <rh26157@gmail.com>
2022-12-17 10:34:40 +0300

    add plain formatter

--- gendiff/formatter.py
+++ gendiff/formatter.py
@@ -1,6 +1,9 @@
 from gendiff.formatters.stylish import display_diff as display_diff_stylish
+from gendiff.formatters.plain import display_diff as display_diff_plain
 
 
 def format_with_formatter(formatter, diff):
     if formatter == 'stylish':
         return display_diff_stylish(diff)
+    elif formatter == 'plain':
+        return display_diff_plain(diff)
---
+++ gendiff/formatters/plain.py
@@ -0,0 +1,38 @@
+def transform(value):
+    if value in ['true', 'false', 'null']:
+        return value
+    return f"'{value}'"
+
+
+def display_diff(diff):     # noqa: C901
+    result = []
+
+    def walk(data, path=''):
+        for x in data:
+            path += f".{x['key']}" if path else f"{x['key']}"
+
+            if isinstance(x.get('value'), dict):
+                x['value'] = '[complex value]'
+            elif isinstance(x.get('value'), str):
+                x['value'] = transform(x['value'])
+
+            if x['status'] == 'nested':
+                walk(x['children'], path)
+            elif x['status'] == 'removed':
+                result.append(f"Property '{path}' was removed")
+            elif x['status'] == 'added':
+                result.append(f"Property '{path}' was added "
+                              + f"with value: {x['value']}")
+            elif x['status'] == 'changed':
+                x['value'][0] = '[complex value]' \
+                    if isinstance(x.get('value')[0], dict) \
+                    else transform(x['value'][0])
+                x['value'][1] = '[complex value]' \
+                    if isinstance(x.get('value')[1], dict) \
+                    else transform(x['value'][1])
+
+                result.append(f"Property '{path}' was updated. "
+                              + f"From {x['value'][0]} to {x['value'][1]}")
+            path = '.'.join(path.split('.')[:-1])
+    walk(diff)
+    return '\n'.join(result)
