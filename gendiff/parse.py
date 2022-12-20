6b83727fe5c05b48b6519c2d7dff289f5fc87d4a
Ruslan Zinnatov <rh26157@gmail.com>
2022-12-20 20:53:16 +0300

    update parse

--- gendiff/parse.py
+++ gendiff/parse.py
@@ -2,8 +2,14 @@ import json
 import yaml
 
 
-def open_file(file):
-    if file.endswith('json'):
-        return json.load(open(file))
-    else:
-        return yaml.load(open(file), Loader=yaml.Loader)
+def open_file(path_to_file):
+    file_name, format_ = path_to_file.split('.')
+    with open(path_to_file) as file:
+        return load_file(file, format_)
+
+
+def load_file(file, format_):
+    if format_ == 'json':
+        return json.load(file)
+    elif format_ == 'yaml' or 'yaml':
+        return yaml.load(file, Loader=yaml.Loader)
