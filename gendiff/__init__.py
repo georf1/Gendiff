718790c23ea3c4c39807af3d2e8c8d2f7408ed73
Ruslan Zinnatov <rh26157@gmail.com>
2022-12-20 20:52:45 +0300

    update gendiff

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,4 +1,4 @@
-from gendiff.scripts.gendiff import generate_diff
+from gendiff.generate_diff_engine import generate_diff
 
 
 __all__ = (generate_diff)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,25 +1,13 @@
 #!/usr/bin/env python
-from gendiff.get_diff_engine import get_diff
-from gendiff.cli import display_cli
-from gendiff.formatter import format_with_formatter
-
-
-def generate_diff(first_file=None, second_file=None, formatter='stylish'):
-    if first_file and second_file:
-        diff = get_diff(first_file, second_file)
-        result = format_with_formatter(formatter, diff)
-
-        return result
-    else:
-        first_file, second_file, formatter = display_cli()
-        diff = get_diff(first_file, second_file)
-        result = format_with_formatter(formatter, diff)
-
-        print(result)
+from gendiff.cli import parse_args
+from gendiff.generate_diff_engine import generate_diff
 
 
 def main():
-    generate_diff()
+    args = parse_args()
+    diff = generate_diff(args.first_file, args.second_file,
+                         args.FORMAT)
+    print(diff)
 
 
 if __name__ == '__main__':
