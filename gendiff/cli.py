81252d5174368aedc069282f8a57cb848a1fb909
Ruslan Zinnatov <rh26157@gmail.com>
2022-12-20 20:53:02 +0300

    update cli

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -4,7 +4,7 @@ import argparse
 DESCRIPTION = 'Compares two configuration files and shows a difference.'
 
 
-def display_cli():
+def parse_args():
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument('first_file', metavar='first_file', type=str)
     parser.add_argument('second_file', metavar='second_file', type=str)
@@ -12,5 +12,4 @@ def display_cli():
                         default='stylish', type=str,
                         help='set format of output')
 
-    args = parser.parse_args()
-    return args.first_file, args.second_file, args.FORMAT
+    return parser.parse_args()
