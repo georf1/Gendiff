1951f1afdc56ccc9b61f14e3229151e9cdfb3b63
Ruslan Zinnatov <rh26157@gmail.com>
2022-12-22 16:09:53 +0300

    update tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,3 +1,4 @@
+import pytest
 from gendiff.generate_diff_engine import generate_diff
 
 
@@ -10,24 +11,14 @@ correct_diff_plain2 = open('tests/fixtures/diff4.txt').read()
 correct_diff_json = open('tests/fixtures/diff5.txt').read()
 
 
-def test_stylish1():
-    diff = generate_diff('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yml')
-    assert diff == correct_diff_stylish1
+@pytest.mark.parametrize("file1,file2,expected", [('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yml', correct_diff_stylish1), ('tests/fixtures/file1.json', 'tests/fixtures/file2.json', correct_diff_stylish2)])
+def test_stylish(file1, file2, expected):
+    assert generate_diff(file1, file2) == expected
 
 
-def test_stylish2():
-    diff = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
-    assert diff == correct_diff_stylish2
-
-
-def test_plain1():
-    diff = generate_diff('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yml', 'plain')
-    assert diff == correct_diff_plain1
-
-
-def test_plain2():
-    diff = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'plain')
-    assert diff == correct_diff_plain2
+@pytest.mark.parametrize("file1,file2,expected", [('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yml', correct_diff_plain1), ('tests/fixtures/file1.json', 'tests/fixtures/file2.json', correct_diff_plain2)])
+def test_plain(file1, file2, expected):
+    assert generate_diff(file1, file2, 'plain') == expected
 
 
 def test_json():
