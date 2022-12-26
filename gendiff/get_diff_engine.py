5cae99b4e8abbbf14c7ca48f9e4a5d86fa14f59b
Ruslan Zinnatov <rh26157@gmail.com>
2022-12-26 14:25:08 +0300

    update get_dif_engine

--- gendiff/get_diff_engine.py
+++ gendiff/get_diff_engine.py
@@ -1,31 +1,28 @@
-def get_diff(first_object, second_object):      # noqa: C901
-    def walk(structure1, structure2):
-        diff = []
+def get_diff(structure1, structure2):      # noqa: C901
+    diff = []
 
-        for key in sorted(structure1 | structure2):
-            if key in structure1 and key not in structure2:
-                diff.append({'key': key,
-                             'value': structure1[key],
-                             'status': 'removed'})
-            elif key in structure2 and key not in structure1:
-                diff.append({'key': key,
-                             'value': structure2[key],
-                             'status': 'added'})
-            elif isinstance(structure1.get(key), dict) \
-                    and isinstance(structure2.get(key), dict):
-                children = walk(structure1[key], structure2[key])
-                diff.append({'key': key,
-                             'status': 'nested',
-                             'children': children})
-            elif structure1.get(key) != structure2.get(key):
-                diff.append({'key': key,
-                             'value': [structure1[key], structure2[key]],
-                             'status': 'changed'})
-            elif key in structure2 \
-                    and structure2.get(key) == structure1.get(key):
-                diff.append({'key': key,
-                             'value': structure2[key],
-                             'status': 'unchanged'})
+    for key in sorted(structure1 | structure2):
+        if key not in structure2:
+            diff.append({'key': key,
+                         'value': structure1[key],
+                         'status': 'removed'})
+        elif key not in structure1:
+            diff.append({'key': key,
+                         'value': structure2[key],
+                         'status': 'added'})
+        elif isinstance(structure1[key], dict) \
+                and isinstance(structure2[key], dict):
+            children = get_diff(structure1[key], structure2[key])
+            diff.append({'key': key,
+                         'status': 'nested',
+                         'children': children})
+        elif structure1[key] != structure2[key]:
+            diff.append({'key': key,
+                         'value': [structure1[key], structure2[key]],
+                         'status': 'changed'})
+        elif structure2[key] == structure1[key]:
+            diff.append({'key': key,
+                         'value': structure2[key],
+                         'status': 'unchanged'})
 
-        return diff
-    return walk(first_object, second_object)
+    return diff
